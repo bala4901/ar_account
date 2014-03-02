92899a9f60569fc5a6bcee9c0d36e94aa6b43466
Andhitia Rama <andhitia.r@gmail.com>
2014-03-02 13:53:34 +0700

    Revisi pada wizard Bank Payment
    1. Description pd bank payment hasil sekarang sdh mengikuti description pd wizard
    2. Partner diisi sesuai dengan partner dari invoice

--- wizard/wizard_bank_payment.py
+++ wizard/wizard_bank_payment.py
@@ -138,7 +138,7 @@ class wizard_bank_payment(osv.osv_memory):
         
         val_header = {
                         'journal_id' : wizard['journal_id'][0],
-                        'name' : wizard['name'][0],
+                        'name' : wizard['name'],
                         'date' : wizard['date'],
                         'account_id' : journal.default_debit_account_id.id,
                         'voucher_type_id' : voucher_type.id,
@@ -168,9 +168,10 @@ class wizard_bank_payment(osv.osv_memory):
                             'voucher_id' : new_account_bank_payment_id,
                             'account_id' : move_line.account_id.id,
                             'move_line_id' : move_line.id,
-                            'name' : move_line.name,
+                            'name' : wizard['name'],
                             'amount' : amount,
                             'type' : 'dr',
+                            'partner_id' : move_line.partner_id.id,
                             }
                     new_account_bank_payment_detail_id = obj_account_voucher_line.create(cr, uid, val, context)
 
