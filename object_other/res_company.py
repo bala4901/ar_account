8506719fae7680e4fb948027f44818a1c6135894
Andhitia Rama <andhitia.r@gmail.com>
2014-03-23 23:09:54 +0700

    hotfix

--- object_other/res_company.py
+++ object_other/res_company.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
 #
-#    OpenERP, Open Source Management Solution	
+#    OpenERP, Open Source Management Solution   
 #    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>). All Rights Reserved
 #    $Id$
 #
@@ -24,12 +24,14 @@
 from osv import fields, osv
 
 class res_company(osv.osv):
-	_name = 'res.company'
-	_inherit = 'res.company'
+    _name = 'res.company'
+    _inherit = 'res.company'
 
-	_columns =	{
-							'account_writeoff_id' : fields.many2one(string='Default Write-Off Account', obj='account.account', domain=['|',('type','!=','view'),('type','!=','closed'),('type','!=','consolidation')]),
-							}
+    _columns =  {
+                'account_writeoff_id' : fields.many2one(string='Default Write-Off Account', obj='account.account', domain=['|',('type','!=','view'),('type','!=','closed'),('type','!=','consolidation')]),
+                'account_internal_transfer_id' : fields.many2one(string='Default Internal Transfer Account', obj='account.account'),
+                'sequence_internal_transfer_id' : fields.many2one(string='Internal Transfer Sequence', obj='ir.sequence'),
+                }
 res_company()
 
 
